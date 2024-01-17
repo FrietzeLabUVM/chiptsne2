85863e5a1cb79db64beefb05b64a4b1ca339870c
jrboyd <jrboyd@uvm.edu>
2024-01-17 15:06:02 -0500

    added bfcif and related

--- R/functions_bfcif.R
+++ R/functions_bfcif.R
@@ -1,11 +1,10 @@
-
 #' bfcif
 #'
 #' Conditionally runs a function if it's results don't exist in the cache.  Cache is controlled by BiocFileCache.
 #'
-#' @param bfc A BiocFileCache object, typically from BiocFileCache::BiocFileCache().
-#' @param rname The unique identifier for the results in the cache. The recommendation is to use either a unique and meaningful description or digest::digest() on a list containing FUN and it's parameters.
 #' @param FUN A function that takes zero arguments.  This function can be a wrapper around other functions; ie. FUN = function(){mean(x)}.
+#' @param rname The unique identifier for the results in the cache. The recommendation is to use either a unique and meaningful description or digest::digest() on a list containing FUN and it's parameters.
+#' @param bfc A BiocFileCache object, typically from BiocFileCache::BiocFileCache().
 #' @param version A version indicator string to further distinguish cache entries.  Typically, you want to iterate this value when code outside of FUN or input parameters have changed and you want to force FUN to reevaluate. Default is SQC_CACHE_VERSION option or v3 if option is not set.
 #' @param force_overwrite If TRUE, FUN will be rerun regardless of cache state. If it exists, current cache contents will be overwritten. Default is SQC_FORCE_CACHE_OVERWRITE option or FALSE if option is not set.
 #' @param return_path_only If TRUE, FUN will not be run and instead the cache path is returned. Default is FALSE.
@@ -17,60 +16,72 @@
 #'
 #' @examples
 #' bfc = BiocFileCache::BiocFileCache()
-#' bfcif(bfc, "test1", function(x)mean(seq(10)), verbose = TRUE)
-#' bfcif(bfc, "test1", function(x)mean(seq(10)), verbose = TRUE,
+#' test_fun = function(x)mean(seq(10))
+#' bfcif(test_fun, "test1", bfc, verbose = TRUE)
+#' bfcif(test_fun, "test1", bfc, verbose = TRUE,
 #'   return_path_only = TRUE)
-#' bfcif(bfc, "test1", function(x)mean(seq(10)), verbose = TRUE,
+#' bfcif(test_fun, "test1", bfc, verbose = TRUE,
 #'   force_overwrite = TRUE)
-bfcif = function(bfc, rname, FUN,
-                 version = getOption("SQC_CACHE_VERSION", "v4"),
-                 force_overwrite = getOption("SQC_FORCE_CACHE_OVERWRITE", FALSE),
-                 return_path_only = FALSE,
-                 verbose = getOption("SQC_CACHE_VERBOSE", FALSE)){
+bfcif = function(FUN, rname, bfc = NULL, version = NULL, force_overwrite = FALSE, return_path_only = FALSE, verbose = TRUE){
+    if(is.null(bfc)){
+        bfc = BiocFileCache::BiocFileCache()
+    }
+    if(!is.null(version)){
+        rname = paste0(rname, "_", version)
+    }
     # is rname in cache?
-    vrname = paste0(rname, "_", version)
-    if(nrow(BiocFileCache::bfcquery(bfc, query = vrname, field = "rname")) == 0){
-        if(verbose) message("results not in cache. ")
-        cache_path = BiocFileCache::bfcnew(bfc, rname = vrname)
+    if(nrow(BiocFileCache::bfcquery(bfc, query = rname, field = "rname", exact = TRUE)) == 0){
+        if(verbose) message("results not in cache. ", appendLF = FALSE)
+        cache_path = BiocFileCache::bfcnew(bfc, rname = rname)
+
     }else{
-        if(verbose) message("previous cache results found. ")
-        cache_path = BiocFileCache::bfcrpath(bfc, vrname)
+        if(verbose) message("previous cache results found. ", appendLF = FALSE)
+        cache_path = BiocFileCache::bfcrpath(bfc, rname)
     }
     if(return_path_only){
         if(verbose) message("returning cache path.")
-        if(verbose) message(cache_path)
         return(cache_path)
     }
     # does cached file exist?
     if(file.exists(cache_path) && !force_overwrite){
         if(verbose) message("loading previous cache results...")
-        if(verbose) message(cache_path)
-        load(cache_path)
+        load(BiocFileCache::bfcrpath(bfc, rname))
     }else{
-        if(verbose) message("running function...")
+        if(verbose) message("running function...", appendLF = FALSE)
         res = FUN()
         if(verbose) message("caching results...")
-        if(verbose) message(cache_path)
         save(res, file = cache_path)
     }
     # return either new results or cached results
-    if(is(res, "data.table")){
-        data.table::setalloccol(res)
-    }
-    if(is.list(res)){
-        if(any(sapply(res, is, class2 = "data.table"))){
-            res = lapply(res, function(x){
-                if(is(x, "data.table")){
-                    data.table::setalloccol(x)
-                }
-            })
-        }
-    }
     res
 }
 
-new_cache = function(cache_path = getOption("SQC_CACHE_PATH", "~/.cache")){
-    BiocFileCache::BiocFileCache(cache_path)
+#' get_args
+#'
+#' @param env environment to capture variables from. Default of `parent.frame()` retrieves the call function environment.
+#' @param to_ignore character vector of variable names to omit. Default of `character()` is nn variables.
+#' @param ... named extra variabes to include.
+#'
+#' @return named list or variables and their values in `env`
+#' @export
+#'
+#' @examples
+#' test_fun = function(x = 10, ...){
+#'     args = get_args(...)
+#'     y = mean(seq(x))
+#'     args
+#' }
+#' test_fun(5)
+#' # example with ...
+#' test_fun(x = 6, y = 7, z = 8)
+#' # example to_ignore
+#' test_fun(x = 6, y = 7, z = 8, to_ignore = "x")
+get_args = function(env = parent.frame(), to_ignore = character(), ...){
+    if(is.function(env)) env = env()
+    stopifnot(is.environment(env))
+    args = c(as.list(env), list(...))
+    args = args[!names(args) %in% to_ignore]
+    args[order(names(args))]
 }
 
 #' digest_args
@@ -81,7 +92,9 @@ new_cache = function(cache_path = getOption("SQC_CACHE_PATH", "~/.cache")){
 #'
 #' @return A md5 digest of all variabes in calling environment. For a function
 #'   is the same as calling arguments.
+#' @importFrom digest digest
 #' @export
+#'
 #'
 #' @examples
 #' #The most common usage is to simply collect all local variables in a function
