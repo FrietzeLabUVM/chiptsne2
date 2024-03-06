cec606cebf2af6d65e253e3ba41f2b53a4c9f186
jrboyd <jrboyd@med.uvm.edu>
2024-03-06 14:27:51 -0500

    rm dev files

--- dev_debug_operator.R
+++
@@ -1,7 +0,0 @@
-ct2.by_treatment = readRDS("../chiptsne2_paper/ct2.by_treatment.Rds")
-e1 = ct2.by_treatment$bza
-e2 = ct2.by_treatment$ctrl
-operator = "-"
-
-ct2.diff =ct2.by_treatment$bza - ct2.by_treatment$ctrl
-colData(ct2.diff)
